b26fe227c2269d49d1c4bdbe1992e12464c17183
Drake Owen <drakeowen@drakes-mbp.lan>
2022-08-05 20:00:50 -0400

    Create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -1,9 +1,9 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index({breads, title}) {
+function Index({ breads, title }) {
     return (
-        <Default title = {title}>
+        <Default title={title}>
             <h2>Index Page</h2>
             <ul>
                 {
@@ -19,6 +19,10 @@ function Index({breads, title}) {
                 }
 
             </ul>
+
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
         </Default>
     )
 }
